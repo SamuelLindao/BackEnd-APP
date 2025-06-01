278019b4a5d0201b23f01939a17873806eb3d5a8
SamuelLindao <lacerdasamuel26@gmail.com>
2025-06-01 14:43:47 -0300

    new get and log

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3001;
 
+app.use(express.json()) // ← Sempre antes das rotas!
+
 app.get('/', (req, res) =>{
   res.send("Olá... Bem-vindo!")
+})
+
+app.post('/filmes', (req,res)=>{
+  const {titulo, genero} = req.body
+  res.send(`Filme:${titulo} Genero: ${genero}, recebido...`)
 })
 
 app.listen(port, () =>{
   console.log(`Servidor rodando em http://localhost:${port}`);
 })
 
-app.use(express.json())
-app.post('/filmes', (req,res)=>{
-  const {titulo, genero} = req.body
-  res.send(`Filme:${titulo}- Genero: ${genero}, recebido...`)
+const log = (req, res, next)=>{
+  console.log(`................. Acessado em ${new Date()}`)
+  next();
+}
+
+app.get('/transfere', log, (req,res) =>{
+  res.send("Ok! Valor Transferido com sucesso...")
 })
